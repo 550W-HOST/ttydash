e7b2c532910fffd129f0a1dda2fb548e27a62db9
PuQing <me@puqing.work>
2024-09-28 21:46:21 +0800

    Refactor main.rs to remove unnecessary return statement

--- src/main.rs
+++ src/main.rs
@@ -18,5 +18,7 @@ async fn main() -> Result<()> {
     let args = Cli::parse();
     let mut app = App::new(args.tick_rate, args.frame_rate, args.title, args.unit)?;
     app.run().await?;
+
+    #[allow(clippy::needless_return)]
     Ok(())
 }
